e0f2d0ac1c1098b19ea393a5fb4124d04ed229bb
ali <al6593@nyu.edu>
2021-02-07 17:44:25 -0500

    Adding Unity Project

---
+++ week 1/Assets/Scripts/WASDController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDController : MonoBehaviour
+{
+
+    public float forceAmt = 10;
+
+    Rigidbody rb; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //calls out to the component Rigidbody attached to Player gameobject. 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) //press W every frame
+        {
+            rb.AddForce(Vector3.forward * forceAmt); //controls physics of the player. gentle force
+        }
+    }
+}
